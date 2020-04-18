200bdb21b21823e4bebeca7c7184b272b81becd0
Sahil Shaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2020-04-18 21:50:55 +0530

    profile face working now

---
+++ haar-cascade/face_detect.py
@@ -0,0 +1,28 @@
+import cv2
+
+front_face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+profile_face_cascade = cv2.CascadeClassifier("haarcascade_profileface.xml")
+
+videoCapture = cv2.VideoCapture(0)
+
+scalefactor = 1.3
+
+while 1:
+    ret, pic = videoCapture.read()
+
+    faces = front_face_cascade.detectMultiScale(pic, scalefactor, 5)
+    faces1 = profile_face_cascade.detectMultiScale(pic, scalefactor, 5)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(pic,(x , y),(x+w , y+h), (0,255,255), 2)
+
+    for(x,y,w,h) in faces1:
+        cv2.rectangle(pic,(x , y),(x+w , y+h), (0,255,255), 2)
+
+    print("number of faces found {}",format(len(faces)))
+    cv2.imshow("Face_Detection", pic)
+    k = cv2.waitKey(30) & 0xff
+    if k==2:
+        break
+
+cv2.destroyAllWindows()
