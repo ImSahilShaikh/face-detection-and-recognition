e5c6345dd076e8f96779946c3b6397123af929cd
Sahil Shaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2020-05-14 16:28:44 +0530

    Update mtcnn_real_time.py

--- mtcnn-realtime-video/mtcnn_real_time.py
+++ mtcnn-realtime-video/mtcnn_real_time.py
@@ -3,8 +3,8 @@ from mtcnn.mtcnn import MTCNN
 import cv2
 detector = MTCNN()
 
-cap = cv2.VideoCapture(0)
-conf_t = 0.99
+cap = cv2.VideoCapture('/media/imsahil/Windows/Users/ImSahil/Downloads/Friends Season 3 Complete 720p.BRrip.Sujaidr (pimprg)/friends.s03e01.720p.bluray.sujaidr.mkv')
+conf_t = 0.97
 while cap.isOpened(): 
     #Capture frame-by-frame
     ret, frame = cap.read()
@@ -18,6 +18,7 @@ while cap.isOpened():
       x1, y1, width, height = res['box']
       x1, y1 = abs(x1), abs(y1)
       x2, y2 = x1 + width, y1 + height
+    
 
       confidence = res['confidence']
       if confidence < conf_t:
@@ -30,6 +31,7 @@ while cap.isOpened():
       for point in key_points:
         cv2.circle(frame, point, 5, (0, 255, 0), thickness=-1)
 
+      print("Number of faces found {}",format(len(results)))
       cv2.imshow('Face-Detection-Using-MTCNN', frame)
       if cv2.waitKey(1) & 0xFF == ord('q'):
         break
