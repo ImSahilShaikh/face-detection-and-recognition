f659bcbd7a14a17b4e8f8bd24238161e0d1b296e
Sahil Shaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2020-05-15 19:19:09 +0530

    Update and rename gui_data_collection.py to image_dataset_gathering/gui_data_collection.py

--- image_dataset_gathering/gui_data_collection.py
+++ image_dataset_gathering/gui_data_collection.py
@@ -1,33 +1,44 @@
 from tkinter import *
 from tkinter import messagebox
 import cv2
-import os
+from random import randint
+from PIL import ImageTk ,Image
+
 
 def capture():
     cam = cv2.VideoCapture(0)
     i=0
-    while True:
-        ret, frame = cam.read()
-        cv2.imshow("Test",frame)
+    val=1
+    while cam.isOpened():
         messagebox.showinfo("Info","Please look directly into camera and press ok")
         for i in range (15):
-            cv2.imwrite('img_data/img{:>03}.jpg'.format(i),frame)
+            ret, frame = cam.read()
+            cv2.imwrite('img_data/'+str(val)+'.jpg',frame)
+            val += 1
         
         messagebox.showinfo("Info","Please tilt you head towards left and press ok")
-        for j in range(i+15):
-            cv2.imwrite('img_data/img{:>03}.jpg'.format(j),frame)
+        for j in range(i,i+15):
+            ret, frame = cam.read()
+            cv2.imwrite('img_data/'+str(val)+'.jpg',frame)
+            val += 1
         
         messagebox.showinfo("Info","Please tilt you head down and press ok")
-        for k in range(j+15):
-            cv2.imwrite('img_data/img{:>03}.jpg'.format(k),frame)     
+        for k in range(j,j+15):
+            ret, frame = cam.read()
+            cv2.imwrite('img_data/'+str(val)+'.jpg',frame) 
+            val += 1
         
         messagebox.showinfo("Info","Please tilt you head towards right and press ok")
-        for l in range(k+15):
-            cv2.imwrite('img_data/img{:>03}.jpg'.format(l),frame)
+        for l in range(k,k+15):
+            ret, frame = cam.read()
+            cv2.imwrite('img_data/'+str(val)+'.jpg',frame)
+            val += 1
         
         messagebox.showinfo("Info","Please tilt you head up and press ok")
-        for m in range(l+15):
-            cv2.imwrite('img_data/img{:>03}.jpg'.format(m),frame)
+        for m in range(l,l+15):
+            ret, frame = cam.read()
+            cv2.imwrite('img_data/'+str(val)+'.jpg',frame)
+            val += 1
 
         cam.release()
         if cv2.waitKey(30) & 0xFF == ord('q'):
@@ -39,11 +50,17 @@ def terminate():
     exit()
 
 myframe = Tk(className = " Window")
-myframe.geometry("500x250")
+myframe.geometry("720x650")
 myframe['background'] = '#f5f5f5'
 
-capture = Button(myframe, text = "Capture", command = capture, activebackground  = '#3E4149')
+
+img=ImageTk.PhotoImage(Image.open ("capture.jpg"))
+lab=Label(image=img)
+
+capture = Button(myframe, image = img, command = capture, activebackground  = '#3E4149')
 capture.place(x = 50, y = 150)
+capture.pack()
 exit_b = Button(myframe, text = 'Exit', command = terminate)
 exit_b.place(x = 150, y = 150)
+exit_b.pack()
 myframe.mainloop()
