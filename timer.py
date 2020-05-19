41eec9bd5170ee20d97e48b954bd0ac39d269f31
Kalpit Choudhari <52930858+KalpitChoudhari@users.noreply.github.com>
2020-05-19 16:32:03 +0530

    Timer : Updated (Remaning - Manipulation of input val)

--- timer.py
+++ timer.py
@@ -2,25 +2,33 @@ import schedule
 import time
 from tkinter import *
 import tkinter as tk
-cnt = 4
+from tkinter import messagebox
 
-def job():
+def job():	
+	prev_cnt = 10
+	curr_cnt = 8
+
 	master = Tk()
 	master.geometry("400x280")
 	variable1=StringVar()
 
 	def retrieve_input(master):
-		ans = variable1.get()
-		if(ans == "yes"):
-			print("Current count : ", cnt)
+		if(var1.get() == 1 and var2.get() == 0):
+			print("Current count : ", curr_cnt)
 			master.destroy()
+		elif((var1.get() == 0 and var2.get() == 0) or (var1.get() == 1 and var2.get() == 1)):
+			messagebox.showwarning("Bad input","Illegal values, please try again")
+			master.destroy()
+			job()
 		else:
-			print("Previous count : ", cnt)
-			schedule.every(1).minutes.do(job)
+			print("Previous count : ", prev_cnt)
 			master.destroy()
 
-	Label(master, text = "Currnet count is : ").place(x = 130,y = 50)   
-	Label(master, text = cnt).place(x = 250,y = 50)
+	Label(master, text = "Previous count is : ").place(x = 50,y = 50)   
+	Label(master, text = prev_cnt).place(x = 180,y = 50)
+
+	Label(master, text = "Currnet count is : ").place(x = 210,y = 50)   
+	Label(master, text = curr_cnt).place(x = 330,y = 50)
 
 	button1 = tk.Button(master, text="Submit", command=lambda: retrieve_input(master))
 	button1.place(x = 120, y = 140)
@@ -28,12 +36,14 @@ def job():
 	button2 = tk.Button(master, text="QUIT", fg="red", command=master.destroy)
 	button2.place(x = 220, y = 140) 
 
-	count = Label(master, text = "Lock count (yes/ no)? :").place(x = 30,y = 100)  
-	e1 = Entry(master,width = 20,textvariable=variable1).place(x = 180, y = 100)  
+	var1 = IntVar()
+	Checkbutton(master, text="Lock Count", variable=var1).place(x = 30,y = 100)
+	var2 = IntVar()
+	Checkbutton(master, text="Use Previous count", variable=var2).place(x = 180, y = 100)
 
 	mainloop()
-
-schedule.every(1).minutes.do(job)
+	
+schedule.every(0.1).minutes.do(job)
 
 while True:
     schedule.run_pending()
